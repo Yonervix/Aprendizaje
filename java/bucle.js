fe3bc60064ec9219d1615e3c8bc9b95529674640
yonervix <yonervimendoza@gmail.com>
2026-08-17 00:00:48 -0400

    bucle

---
+++ java/bucle.js
@@ -0,0 +1,25 @@
+// for 
+
+for (let i = 5; i <=15; i++ ){
+    console.log(i);
+
+}
+//  
+
+
+// while
+
+let i = 5;
+while(i <=15) {
+    console.log(i * 5);
+    i++;
+}
+
+// while
+
+
+
+
+
+
+
